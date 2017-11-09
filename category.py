4841d52c7f0d26bdd4590588f51f2379bbbbfdb2
meilingjie <meilingjie@xinyunlian.com>
2017-11-09 16:53:56 +0800

    no meaning

--- category.py
+++ category.py
@@ -1,10 +1,11 @@
-# coding：utf-8
+#!/usr/bin/python
+# -*-coding:utf-8 -*-
 import pymysql
 
 
 def category_data_optimize():
-    conn = pymysql.connect(host='192.168.200.152', database='huala_shanxi', user='root',
-                           password='xpsh', charset="utf8")
+    conn = pymysql.connect(host='127.0.0.1', database='test', user='root',
+                           password='123456', charset="utf8")
     count = 0
     cursor = conn.cursor()
     cursor.execute("select id, parent_cid, seller_id from h_seller_goods_cat where parent_cid != 0")
@@ -23,12 +24,11 @@ def category_data_optimize():
             else:
                 cursor.execute("select * from h_goods_cat where id = " + str(data[1]))
                 dp = cursor.fetchone()
-                ss = "INSERT INTO h_seller_goods_cat ( seller_id, cid, cname, cat_img, parent_cid, parent_cname, is_parent, sort, add_time, add_user,is_delete) VALUES ( " + str(
-                    data[2]) + ", " + str(data[1]) + ", " + str(dp[1]) + ", " + str(dp[2]) + ", " + str(
-                    dp[3]) + ", " + str(dp[4]) + ", " + str(dp[5]) + ", " + str(dp[6]) + ", " + str(dp[7]) + ", " + str(
-                    dp[8]) + ", " + str(dp[9]) + ")"
-                count = count + 1
-                cursor.execute(ss)
+                if dp is not None:
+                    ss = "INSERT INTO h_seller_goods_cat ( seller_id, cid, cname, cat_img, parent_cid, parent_cname, is_parent, sort, add_time, add_user,is_delete) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    count = count + 1
+                    cursor.execute(ss,
+                                   (data[2], data[1], dp[1], dp[2], dp[3], dp[4], dp[5], dp[6], dp[7], dp[8], dp[9]))
 
     print("total execute " + str(count) + " record")
     conn.commit()
